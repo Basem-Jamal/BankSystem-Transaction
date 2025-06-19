bd3bc7958b6e4d72215c60a56c3e9e1492d3ebb7
Basem <Basemjamal2019@gmail.com>
2025-06-19 18:05:25 +0300

    origin/main

---
+++ login.js
@@ -0,0 +1,19 @@
+class clsLogin{
+    constructor() {
+
+        this.containerLogin = document.querySelector(".container-SingUP");
+        this.containerSignUp = document.querySelector(".container-SingUP");
+
+        
+    }
+
+    newUsers ()
+    {
+        
+    }
+
+    save ()
+    {
+        
+    }
+}
